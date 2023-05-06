ac8950f256c2eb8c1412419c02c49498bb153500
Yevgeniy Shatalin <shatalin_ei@mail.ru>
2023-05-07 01:15:41 +0600

    React and Redux.Action creators. Working with arrays. Refactoring

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
 import {useDispatch, useSelector} from "react-redux";
+import {addCashAction, getCashAction} from "./store/index/cashReducer";
+import {addCustomerAction, removeCustomerAction} from "./store/index/customersReducer";
 
 function App() {
-  const dispatch = useDispatch()
-  const cash = useSelector(state => state.cash.cash)
+    const dispatch = useDispatch()
+    const cash = useSelector(state => state.cash.cash)
+    const customers = useSelector(state => state.customers.customers)
 
-  const addCash = (cash) => {
-    dispatch({type: "ADD_CASH", payload: cash})
-  }
+    const addCash = (cash) => {
+        dispatch(addCashAction(cash))
+    }
 
-  const getCash = (cash) => {
-    dispatch({type: "GET_CASH", payload: cash})
-  }
+    const getCash = (cash) => {
+        dispatch(getCashAction(cash))
+    }
 
-  return (
-    <div >
-      <div>{cash}</div>
-      <button onClick={() => addCash(Number(prompt()))}>Пополнить</button>
-      <button onClick={() => getCash(Number(prompt()))}>Снять</button>
-    </div>
-  );
+    const addCustomer = (customer) => {
+        let newCustomer = {
+            name: customer,
+            id: Date.now()
+        }
+        dispatch(addCustomerAction(newCustomer))
+    }
+
+    const removeCustomer = (id) => {
+        dispatch(removeCustomerAction(id))
+    }
+
+    return (
+        <div style={{textAlign: 'center'}}>
+            <div style={{padding: 15, margin: 15, fontSize: '2em'}}>{cash}</div>
+            <button
+                style={{padding: 15, margin: 15, fontSize: '2em'}}
+                onClick={() => addCash(Number(prompt()))}>
+                Пополнить
+            </button>
+            <button
+                style={{padding: 15, margin: 15, fontSize: '2em'}}
+                onClick={() => getCash(Number(prompt()))}>
+                Снять
+            </button>
+            <button
+                style={{padding: 15, margin: 15, fontSize: '2em'}}
+                onClick={() => addCustomer((prompt()))}>
+                Добавить пользователя
+            </button>
+            <div>
+                {customers.length === 0 ?
+                    <div>
+                        пользователей нет
+                    </div>
+                    :
+                    <div>
+                        {customers.map(customer =>
+                            <div onClick={() => {removeCustomer(customer.id)}}
+                                 style={{cursor: 'pointer'}}
+                            >
+                                {customer.name}
+                            </div>
+                        )
+                        }
+                    </div>
+                }
+            </div>
+        </div>
+    );
 }
 
 export default App;
